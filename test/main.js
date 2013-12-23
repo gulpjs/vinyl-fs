35f230b2abe22e5fa0cc710a9bbbfb8dd3cfe3fa
Contra <contra@maricopa.edu>
2013-12-23 04:00:47 -0700

    start impl

---
+++ test/main.js
@@ -0,0 +1,13 @@
+var vfs = require('../');
+var should = require('should');
+require('mocha');
+
+describe('vinyl-fs src', function() {
+  describe('src(glob)', function() {
+    it('should emit files', function(done) {
+      should.exist(true);
+      true.should.equal["true"];
+      done();
+    });
+  });
+});
