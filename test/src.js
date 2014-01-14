19eead881a95003a692f64a00d21f9d803b35592
contra <yo@contra.io>
2014-01-14 06:01:07 -0700

    test cleaning

--- test/src.js
+++ test/src.js
@@ -23,19 +23,18 @@ describe('source stream', function() {
       contents: expectedContent
     });
 
-    var stream = vfs.src("./fixtures/nothing.coffee", {cwd: __dirname});
-    var buffered = [];
-
-    stream.on('error', done);
-    stream.on('data', function(file) {
-      buffered.push(file);
-    });
-    stream.on('end', function(){
+    var onEnd = function(){
       buffered.length.should.equal(1);
       buffered[0].should.equal(expectedFile);
       bufEqual(buffered[0].contents, expectedContent).should.equal(true);
       done();
-    });
+    };
+
+    var stream = vfs.src("./fixtures/nothing.coffee", {cwd: __dirname});
+
+    var buffered = [];
+    bufferStream = es.through(buffered.push.bind(buffered), onEnd);
+    stream.pipe(bufferStream);
     stream.write(expectedFile);
   });
 
@@ -43,74 +42,64 @@ describe('source stream', function() {
     var expectedPath = path.join(__dirname, "./fixtures/test.coffee");
     var expectedContent = fs.readFileSync(expectedPath);
 
-    var stream = vfs.src("./fixtures/*.coffee", {cwd: __dirname});
-    var buffered = [];
-
-    stream.on('error', done);
-    stream.on('data', function(file) {
-      buffered.push(file);
-    });
-    stream.on('end', function(){
+    var onEnd = function(){
       buffered.length.should.equal(1);
       should.exist(buffered[0].stat);
       buffered[0].path.should.equal(expectedPath);
       buffered[0].isBuffer().should.equal(true);
       bufEqual(buffered[0].contents, expectedContent).should.equal(true);
       done();
-    });
+    };
+
+    var stream = vfs.src("./fixtures/*.coffee", {cwd: __dirname});
+
+    var buffered = [];
+    bufferStream = es.through(buffered.push.bind(buffered), onEnd);
+    stream.pipe(bufferStream);
   });
 
   it('should glob a directory with default settings', function(done) {
     var expectedPath = path.join(__dirname, "./fixtures/wow");
 
-    var stream = vfs.src("./fixtures/wow/", {cwd: __dirname});
-    var buffered = [];
-
-    stream.on('error', done);
-    stream.on('data', function(file) {
-      buffered.push(file);
-    });
-    stream.on('end', function(){
+    var onEnd = function(){
       buffered.length.should.equal(1);
       buffered[0].path.should.equal(expectedPath);
       buffered[0].isNull().should.equal(true);
       buffered[0].isDirectory().should.equal(true);
       done();
-    });
+    };
+
+    var stream = vfs.src("./fixtures/wow/", {cwd: __dirname});
+
+    var buffered = [];
+    bufferStream = es.through(buffered.push.bind(buffered), onEnd);
+    stream.pipe(bufferStream);
   });
 
   it('should glob a file with read: false', function(done) {
     var expectedPath = path.join(__dirname, "./fixtures/test.coffee");
     var expectedContent = fs.readFileSync(expectedPath);
 
-    var stream = vfs.src("./fixtures/*.coffee", {cwd: __dirname, read: false});
-    var buffered = [];
-
-    stream.on('error', done);
-    stream.on('data', function(file) {
-      buffered.push(file);
-    });
-    stream.on('end', function(){
+    var onEnd = function(){
       buffered.length.should.equal(1);
       buffered[0].path.should.equal(expectedPath);
       buffered[0].isNull().should.equal(true);
       should.not.exist(buffered[0].contents);
       done();
-    });
+    };
+
+    var stream = vfs.src("./fixtures/*.coffee", {cwd: __dirname, read: false});
+
+    var buffered = [];
+    bufferStream = es.through(buffered.push.bind(buffered), onEnd);
+    stream.pipe(bufferStream);
   });
 
   it('should glob a file with buffer: false', function(done) {
     var expectedPath = path.join(__dirname, "./fixtures/test.coffee");
     var expectedContent = fs.readFileSync(expectedPath);
 
-    var stream = vfs.src("./fixtures/*.coffee", {cwd: __dirname, buffer: false});
-    var buffered = [];
-
-    stream.on('error', done);
-    stream.on('data', function(file) {
-      buffered.push(file);
-    });
-    stream.on('end', function(){
+    var onEnd = function(){
       buffered.length.should.equal(1);
       should.exist(buffered[0].stat);
       buffered[0].path.should.equal(expectedPath);
@@ -120,7 +109,13 @@ describe('source stream', function() {
         bufEqual(Buffer(content), expectedContent);
         done();
       }));
-    });
+    };
+
+    var stream = vfs.src("./fixtures/*.coffee", {cwd: __dirname, buffer: false});
+    
+    var buffered = [];
+    bufferStream = es.through(buffered.push.bind(buffered), onEnd);
+    stream.pipe(bufferStream);
   });
 
 });
