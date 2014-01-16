ee32c0a3a5f7aa30fc62a8481dd2f38aa410fa1a
Contra <contra@maricopa.edu>
2014-01-15 19:22:08 -0700

    more simplifying

--- test/dest.js
+++ test/dest.js
@@ -19,7 +19,7 @@ describe('dest stream', function() {
   beforeEach(wipeOut);
   afterEach(wipeOut);
 
-  it('should pass through writes', function(done) {
+  it('should pass through writes with cwd', function(done) {
     var expectedPath = path.join(__dirname, "./out-fixtures/test.coffee");
 
     var expectedFile = new File({
@@ -36,6 +36,31 @@ describe('dest stream', function() {
     };
 
     var stream = vfs.dest("./out-fixtures/", {cwd: __dirname});
+
+    var buffered = [];
+    bufferStream = es.through(buffered.push.bind(buffered), onEnd);
+    stream.pipe(bufferStream);
+    stream.write(expectedFile);
+    stream.end();
+  });
+
+  it('should pass through writes with default cwd', function(done) {
+    var expectedPath = path.join(__dirname, "./out-fixtures/test.coffee");
+
+    var expectedFile = new File({
+      base: __dirname,
+      cwd: __dirname,
+      path: expectedPath,
+      contents: null
+    });
+
+    var onEnd = function(){
+      buffered.length.should.equal(1);
+      buffered[0].should.equal(expectedFile);
+      done();
+    };
+
+    var stream = vfs.dest(path.join(__dirname, "./out-fixtures/"));
 
     var buffered = [];
     bufferStream = es.through(buffered.push.bind(buffered), onEnd);
