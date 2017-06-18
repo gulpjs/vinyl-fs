3ce70d58a446e5305e6231958b47d113215affe6
Blaine Bublitz <blaine.bublitz@gmail.com>
2017-06-18 14:39:23 -0700

    another test and fixes

--- test/dest-symlinks.js
+++ test/dest-symlinks.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var path = require('path');
+
 var fs = require('graceful-fs');
 var File = require('vinyl');
 var expect = require('expect');
@@ -57,6 +59,30 @@ describe.only('.dest() with symlinks', function() {
     pipe([
       from.obj([file]),
       vfs.dest(outputBase),
+      concat(assert),
+    ], done);
+  });
+
+  it('can create relative links', function(done) {
+    var file = new File({
+      base: inputBase,
+      path: inputPath,
+      contents: null,
+    });
+
+    // `src()` adds this side-effect with `resolveSymlinks` option set to false
+    file.symlink = inputPath;
+
+    function assert(files) {
+      var outputLink = fs.readlinkSync(outputPath);
+
+      expect(files.length).toEqual(1);
+      expect(outputLink).toEqual(path.normalize('../fixtures/test.txt'));
+    }
+
+    pipe([
+      from.obj([file]),
+      vfs.dest(outputBase, { relative: true }),
       concat(assert),
     ], done);
   });
