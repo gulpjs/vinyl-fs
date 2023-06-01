027895c5b9c4856dbc73840b5adba2253c268c02
Blaine Bublitz <blaine.bublitz@gmail.com>
2023-05-31 19:20:31 -0700

    owner tests for each stream type

--- test/dest-owner.js
+++ test/dest-owner.js
@@ -4,20 +4,12 @@ var fs = require('graceful-fs');
 var File = require('vinyl');
 var expect = require('expect');
 var sinon = require('sinon');
-var stream = require('stream');
-var concat = require('concat-stream');
-
-// TODO: This `from` should be replaced to `node:stream.Readable.from`
-// if this package supports only >= v10.17.0
-var from = require('streamx').Readable.from;
 
 var vfs = require('../');
 
 var cleanup = require('./utils/cleanup');
 var isWindows = require('./utils/is-windows');
 var testConstants = require('./utils/test-constants');
-
-var pipeline = stream.pipeline;
 
 var inputBase = testConstants.inputBase;
 var outputBase = testConstants.outputBase;
@@ -26,68 +18,71 @@ var contents = testConstants.contents;
 
 var clean = cleanup(outputBase);
 
-describe('.dest() with custom owner', function() {
+function suite(moduleName) {
+  var stream = require(moduleName);
 
-  beforeEach(clean);
-  afterEach(clean);
+  var from = stream.Readable.from;
 
-  it('calls fchown when the uid and/or gid are provided on the vinyl stat', function(done) {
-    if (isWindows) {
-      this.skip();
-      return;
-    }
+  describe('.dest() with custom owner (with ' + moduleName + ')', function () {
+    beforeEach(clean);
+    afterEach(clean);
 
-    var fchownSpy = sinon.spy(fs, 'fchown');
+    it('calls fchown when the uid and/or gid are provided on the vinyl stat', function (done) {
+      if (isWindows) {
+        this.skip();
+        return;
+      }
 
-    var file = new File({
-      base: inputBase,
-      path: inputPath,
-      contents: Buffer.from(contents),
-      stat: {
-        uid: 1001,
-        gid: 1001,
-      },
+      var fchownSpy = sinon.spy(fs, 'fchown');
+
+      var file = new File({
+        base: inputBase,
+        path: inputPath,
+        contents: Buffer.from(contents),
+        stat: {
+          uid: 1001,
+          gid: 1001,
+        },
+      });
+
+      function assert() {
+        expect(fchownSpy.callCount).toEqual(1);
+        expect(fchownSpy.getCall(0).args[1]).toEqual(1001);
+        expect(fchownSpy.getCall(0).args[2]).toEqual(1001);
+        done();
+      }
+
+      stream.pipeline([from([file]), vfs.dest(outputBase)], assert);
     });
 
-    function assert() {
-      expect(fchownSpy.callCount).toEqual(1);
-      expect(fchownSpy.getCall(0).args[1]).toEqual(1001);
-      expect(fchownSpy.getCall(0).args[2]).toEqual(1001);
-    }
+    it('does not call fchown when the uid and gid provided on the vinyl stat are invalid', function (done) {
+      if (isWindows) {
+        this.skip();
+        return;
+      }
 
-    pipeline([
-      from([file]),
-      vfs.dest(outputBase),
-      concat(assert),
-    ], done);
-  });
+      var fchownSpy = sinon.spy(fs, 'fchown');
 
-  it('does not call fchown when the uid and gid provided on the vinyl stat are invalid', function(done) {
-    if (isWindows) {
-      this.skip();
-      return;
-    }
+      var file = new File({
+        base: inputBase,
+        path: inputPath,
+        contents: Buffer.from(contents),
+        stat: {
+          uid: -1,
+          gid: -1,
+        },
+      });
 
-    var fchownSpy = sinon.spy(fs, 'fchown');
+      function assert() {
+        expect(fchownSpy.callCount).toEqual(0);
+        done();
+      }
 
-    var file = new File({
-      base: inputBase,
-      path: inputPath,
-      contents: Buffer.from(contents),
-      stat: {
-        uid: -1,
-        gid: -1,
-      },
+      stream.pipeline([from([file]), vfs.dest(outputBase)], assert);
     });
-
-    function assert() {
-      expect(fchownSpy.callCount).toEqual(0);
-    }
-
-    pipeline([
-      from([file]),
-      vfs.dest(outputBase),
-      concat(assert),
-    ], done);
   });
-});
+}
+
+suite('stream');
+suite('streamx');
+suite('readable-stream');
